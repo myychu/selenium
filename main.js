d9f9a2f07e2a4a3404e4d5c0a0dba62b7f1873f9
ccruz <ccruz@mrhouston.net>
2020-10-13 11:39:48 +0200

    Chapp API updated: * Read/Write from/to database instead of a file. * Improved precision datetime data

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ const buf = require('buffer');
 const sleep = require('sleep');
 const fs = require('fs');
 const str = require('string');
+const mysql = require('mysql');
+const promisify = require('util-promisify');
+const moment = require('moment');
+
+
+// Create the connection to database
+let conn = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'root',
+  database: 'chapp', 
+});
+
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
  
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
@@ -13,24 +31,46 @@ const str = require('string');
     let wstream = fs.createWriteStream(userdata);
     let user = '';
     let pwd = '';
+
+    await driver.get('https://panel.chapp.es/Identity/Account/Login?ReturnUrl=%2F');
+/* Login through a file
+
     fs.readFile(credentials, 'utf8', function(err,data){
       if(err) throw err;
       user = (data.split('user:')[1]).split('pwd')[0];
       pwd = data.split('pwd:')[1];
     });
 
-    // login
-    await driver.get('https://panel.chapp.es/Identity/Account/Login?ReturnUrl=%2F');
     let buff = new buf.Buffer.from(user, 'base64');
     await driver.findElement(By.name('Input.Email')).sendKeys(buff.toString('UTF-8'), Key.TAB);
     buff = new buf.Buffer.from(pwd, 'base64');
     await driver.findElement(By.name('Input.Password')).sendKeys(buff.toString('UTF-8'), Key.RETURN);
+*/
+    // Login through a database
+    const aquery = promisify(conn.query).bind(conn);
+    let result = await aquery("SELECT * FROM user WHERE alias = 'pgarcia'");
+    console.log(result);
+    userId = result[0].id;
+    user = result[0].email;
+    pwd = result[0].pwd;
+    console.log('id: ' +userId);
+
+    console.log(user + ", " + pwd);
+
+    let buff = new buf.Buffer.from(user, 'base64');
+    user = "\"" + buff.toString('UTF-8') + "\"";
+    await driver.findElement(By.name('Input.Email')).sendKeys(buff.toString('UTF-8'), Key.TAB);
+    buff = new buf.Buffer.from(pwd, 'base64');
+    pwd = "\"" + (new buf.Buffer.from(pwd, 'base64')).toString('UTF-8') + "\"";
+    await driver.findElement(By.name('Input.Password')).sendKeys(buff.toString('UTF-8'), Key.RETURN);
+
+    sleep.sleep(5);
 
     // Go to Jornadas
     await driver.findElement(By.linkText('Jornadas')).click();
 
     // Wait until the page charges
-    sleep.sleep(10);
+    sleep.sleep(15);
 
     // Keep worked time elements on this page
     let userEmail = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[1]/td[1]/span")).getText();
@@ -40,6 +80,9 @@ const str = require('string');
     let nextElement = await driver.findElement(By.xpath("//*[@id='tableJornadas_next']"));
     let nextClass = await nextElement.getAttribute('class');
     console.log(nextClass);
+
+    // Delete previous entries
+    await aquery("DELETE FROM entry WHERE userId = ?", [userId]);
 
     while (!str(nextClass).endsWith('disabled')) {
       sleep.sleep(10);
@@ -51,16 +94,28 @@ const str = require('string');
       totalEntries = (lastDigit - firstDigit) + 1;
       console.log(totalEntries);
 
+      // Queries to make inserts in database
+      let sql_entry = 'INSERT INTO entry (date, worked, stopped, total, userId) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE date = ?, worked = ?, stopped = ?, total = ?, userId = ?';
+      let sql_userEntry = 'INSERT INTO userentry (userId, entryId) VALUES (?, ?) ON DUPLICATE KEY UPDATE userId = ?, entryId = ?';
+
       // Look for each entry and save it in a file
       for (let i = 1; i <= totalEntries; i++){
-        let date = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[2]")).getText();
-        date = date.slice(0, date.indexOf(' '));
+        console.log('NEW ENTRY!!!!! -----------------------')
+        let entireDate = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[2]")).getText();
+        // Convert date from DD/MM/YYYY to YYYY/MM/DD
+        date = entireDate.slice(0, entireDate.indexOf(' '));
+        date = date.split("/").reverse().join("/");
+        time = entireDate.slice(entireDate.indexOf(' '), entireDate.length);
+        entireDate = date + time;
+
         let worked = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[4]/span")).getText();
         let stopped = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[5]/span")).getText();
         let total = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[6]/span")).getText();
-       
-        wstream.write ('Date: ' + date + ', worked: ' + worked + ', stopped: ' + stopped + ', total: ' + total + '\n');
 
+        wstream.write ('Date: ' + entireDate + ', worked: ' + worked + ', stopped: ' + stopped + ', total: ' + total + '\n');
+
+        // Create elements and keep them into database
+        result = await aquery(sql_entry, [entireDate, worked, stopped, total, userId, entireDate, worked, stopped, total, userId]);
       };
 
       // Check the status of next page button
@@ -70,6 +125,7 @@ const str = require('string');
     }
 
     wstream.end();
+    conn.end();
     // fs.close();
 
   } finally {
