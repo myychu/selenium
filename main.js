277e7773da4ea4c086f3491bee8cf054724c8087
ccruz <ccruz@mrhouston.net>
2020-10-15 12:30:21 +0200

    Removed selenium functionality for calling Chapp endpoint to obtain records

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key} = require('selenium-webdriver');
 const buf = require('buffer');
 const sleep = require('sleep');
-const fs = require('fs');
-const str = require('string');
 const mysql = require('mysql');
 const promisify = require('util-promisify');
-const moment = require('moment');
+const axios = require('axios').default;
+const querystring = require('querystring');
 
 
 // Create the connection to database
@@ -25,38 +24,21 @@ conn.connect(function(err) {
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    // variables
-    let userdata = 'output.txt';
-    let credentials = 'credentials.txt';
-    let wstream = fs.createWriteStream(userdata);
     let user = '';
     let pwd = '';
 
     await driver.get('https://panel.chapp.es/Identity/Account/Login?ReturnUrl=%2F');
-/* Login through a file
 
-    fs.readFile(credentials, 'utf8', function(err,data){
-      if(err) throw err;
-      user = (data.split('user:')[1]).split('pwd')[0];
-      pwd = data.split('pwd:')[1];
-    });
-
-    let buff = new buf.Buffer.from(user, 'base64');
-    await driver.findElement(By.name('Input.Email')).sendKeys(buff.toString('UTF-8'), Key.TAB);
-    buff = new buf.Buffer.from(pwd, 'base64');
-    await driver.findElement(By.name('Input.Password')).sendKeys(buff.toString('UTF-8'), Key.RETURN);
-*/
     // Login through a database
     const aquery = promisify(conn.query).bind(conn);
-    let result = await aquery("SELECT * FROM user WHERE alias = 'pgarcia'");
+    const alias = "ccruz";
+    let result = await aquery("SELECT * FROM user WHERE alias = ?", [alias]);
     console.log(result);
     userId = result[0].id;
     user = result[0].email;
     pwd = result[0].pwd;
-    console.log('id: ' +userId);
 
-    console.log(user + ", " + pwd);
-
+    // Parse user and pwd from base64 to UTF-8 to use it in page inputs
     let buff = new buf.Buffer.from(user, 'base64');
     user = "\"" + buff.toString('UTF-8') + "\"";
     await driver.findElement(By.name('Input.Email')).sendKeys(buff.toString('UTF-8'), Key.TAB);
@@ -64,71 +46,116 @@ conn.connect(function(err) {
     pwd = "\"" + (new buf.Buffer.from(pwd, 'base64')).toString('UTF-8') + "\"";
     await driver.findElement(By.name('Input.Password')).sendKeys(buff.toString('UTF-8'), Key.RETURN);
 
-    sleep.sleep(5);
-
-    // Go to Jornadas
-    await driver.findElement(By.linkText('Jornadas')).click();
-
-    // Wait until the page charges
-    sleep.sleep(15);
-
-    // Keep worked time elements on this page
-    let userEmail = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[1]/td[1]/span")).getText();
-    wstream.write('Entries made by ' + userEmail + ':\n');
-
-    //Keep next button value class
-    let nextElement = await driver.findElement(By.xpath("//*[@id='tableJornadas_next']"));
-    let nextClass = await nextElement.getAttribute('class');
-    console.log(nextClass);
-
+    //SQL
     // Delete previous entries
     await aquery("DELETE FROM entry WHERE userId = ?", [userId]);
+    // Queries to make inserts in database
+    let sql_entry = 'INSERT INTO entry (initDate, endDate, worked, stopped, total, userId) VALUES (?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE initDate = ?, endDate = ?, worked = ?, stopped = ?, total = ?, userId = ?';
 
-    while (!str(nextClass).endsWith('disabled')) {
-      sleep.sleep(10);
+    // sleep.sleep(5);
+    let cookies = await driver.manage().getCookies(); 
+    cookies = JSON.stringify(cookies);
+    cookie = cookies.slice(cookies.indexOf(".AspNetCore.Identity.Application"), cookies.length);
+    cookie = cookie.slice(cookie.indexOf("value\":\"") + 8, cookie.indexOf("\"},"))
+    console.log("<---cookie--->\n" + cookie + "\n");
 
-      // Calculate how many entries there are per page
-      let dataTableInfo = await driver.findElement(By.id("tableJornadas_info")).getText();
-      let firstDigit = str(dataTableInfo).between('del ', ' al');
-      let lastDigit = str(dataTableInfo).between('al ', ' de');
-      totalEntries = (lastDigit - firstDigit) + 1;
-      console.log(totalEntries);
-
-      // Queries to make inserts in database
-      let sql_entry = 'INSERT INTO entry (date, worked, stopped, total, userId) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE date = ?, worked = ?, stopped = ?, total = ?, userId = ?';
-      let sql_userEntry = 'INSERT INTO userentry (userId, entryId) VALUES (?, ?) ON DUPLICATE KEY UPDATE userId = ?, entryId = ?';
-
-      // Look for each entry and save it in a file
-      for (let i = 1; i <= totalEntries; i++){
-        console.log('NEW ENTRY!!!!! -----------------------')
-        let entireDate = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[2]")).getText();
-        // Convert date from DD/MM/YYYY to YYYY/MM/DD
-        date = entireDate.slice(0, entireDate.indexOf(' '));
-        date = date.split("/").reverse().join("/");
-        time = entireDate.slice(entireDate.indexOf(' '), entireDate.length);
-        entireDate = date + time;
-
-        let worked = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[4]/span")).getText();
-        let stopped = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[5]/span")).getText();
-        let total = await driver.findElement(By.xpath("//*[@id='tableJornadas']/tbody/tr[" + i + "]/td[6]/span")).getText();
-
-        wstream.write ('Date: ' + entireDate + ', worked: ' + worked + ', stopped: ' + stopped + ', total: ' + total + '\n');
-
-        // Create elements and keep them into database
-        result = await aquery(sql_entry, [entireDate, worked, stopped, total, userId, entireDate, worked, stopped, total, userId]);
-      };
-
-      // Check the status of next page button
-      nextElement = await driver.findElement(By.xpath("//*[@id='tableJornadas_next']"));
-      nextClass = await nextElement.getAttribute('class');
-      await nextElement.click();  
+    let data = {
+      'draw': 3,
+      'columns[0][data]': 0,
+      'columns[0][name]': '',
+      'columns[0][searchable]': false,
+      'columns[0][orderable]': false,
+      'columns[0][search][value]': '',
+      'columns[0][search][regex]': false,
+      'columns[1][data]': 1,
+      'columns[1][name]': '',
+      'columns[1][searchable]': false,
+      'columns[1][orderable]': false,
+      'columns[1][search][value]': '',
+      'columns[1][search][regex]': false,
+      'columns[1][data]': 2,
+      'columns[2][name]': '',
+      'columns[2][searchable]': false,
+      'columns[2][orderable]': false,
+      'columns[2][search][value]': '',
+      'columns[2][search][regex]': false,
+      'columns[3][data]': 3,
+      'columns[3][name]': '',
+      'columns[3][searchable]': false,
+      'columns[3][orderable]': false,
+      'columns[3][search][value]': '',
+      'columns[3][search][regex]': false,
+      'columns[4][data]': 4,
+      'columns[4][name]': '',
+      'columns[4][searchable]': false,
+      'columns[4][orderable]': false,
+      'columns[4][search][value]': '',
+      'columns[4][search][regex]': false,
+      'columns[5][data]': 5,
+      'columns[5][name]': '',
+      'columns[5][searchable]': false,
+      'columns[5][orderable]': false,
+      'columns[5][search][value]': '',
+      'columns[5][search][regex]': false,
+      'columns[6][data]': 6,
+      'columns[6][name]': '',
+      'columns[6][searchable]': false,
+      'columns[6][orderable]': false,
+      'columns[6][search][value]': '',
+      'columns[6][search][regex]': false,
+      'order[0][column]': 1,
+      'order[0][dir]': 'desc',
+      'start': 0,
+      'length': 1000,
+      'search[value]': '',
+      'search[regex]': false
     }
 
-    wstream.end();
+    let dataset = await axios({
+      method: 'post',
+      url: 'https://panel.chapponline.es/Jornada/GetJornadasAjax/',
+      headers: {
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "es-ES,es;q=0.9",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        'cookie': ".AspNetCore.Identity.Application=" + cookie},
+      data: querystring.encode(data)
+    });
+
+    dataset = dataset.data.data;
+
+    console.log("entries made by " + user)
+    for(let i = 0; i < dataset.length; i++){
+      let initDate = parseDate(dataset[i].inicio);
+      let endDate = parseDate(dataset[i].fin);
+      let worked = dataset[i].trabajado;
+      let stopped = dataset[i].parado;
+      let total = dataset[i].total;
+
+      // Create elements and keep them into database
+      console.log(userId + " \ " + initDate + " \ " + endDate + " \ " + worked + " \ " + stopped + " \ " + total);
+      result = await aquery(sql_entry, [initDate, endDate, worked, stopped, total, userId, initDate, endDate, worked, stopped, total, userId]);
+    }
+    
     conn.end();
-    // fs.close();
 
   } finally {
-    // await driver.quit();
+    await driver.quit();
   }
 })();
+
+function parseDate(entireDate){
+  if (entireDate != "-"){
+    date = entireDate.slice(0, entireDate.indexOf(' '));
+    date = date.split("/").reverse().join("/");
+    time = entireDate.slice(entireDate.indexOf(' '), entireDate.length);
+    entireDate = date + time;
+    return entireDate;
+  }else{
+    return null;
+  }
+}
